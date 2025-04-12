0c76fc127f449651e3394826fff35a2c926829d6
dineshcrd <cloudtechnology129@gmail.com>
2025-04-12 08:37:17 +0530

    Bash Scripting

--- 04-Variable.sh
+++ 04-Variable.sh
@@ -10,5 +10,5 @@ echo "a + b = $((a + b))"
 TodaysDate=2025-10-01
 echo "Today's date is $TodaysDate"
 
-TodaysDate=Sat Apr 12 03:04:24 UTC 2025
+TodaysDate=Apr 12 2025
 echo "Today's date is $TodaysDate"
