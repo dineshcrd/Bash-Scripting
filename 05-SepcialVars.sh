cf44c54064bf8f1ce32a7d648f1d6a548be63267
dineshcrd <cloudtechnology129@gmail.com>
2025-04-12 08:48:07 +0530

    Bash Scripting

--- 05-SepcialVars.sh
+++ 05-SepcialVars.sh
@@ -1,12 +1,16 @@
-#!/bin/bash
+#!/bin/bash 
 
-a=1000
+# Special Variables are the pre-defined variables with specific special pre-defined powers.
+
+# Special Variables are $0 to $9, $* , $@, $# ,  $$
+# Can you create special variables on Bash ? 100% No , you can just use the special variables that are offered.
+
+a=1000 
 TEAM=$1
 PROJECT=$2
-
-echo "The value of a is $a"
-echo "Name of the TEAM is $TEAM"
-echo "Name of the PROJECT is $PROJECT"
-echo "script name is $0"
-echo "Number of arguments in script are $#"
-echo "Number of arguments in script are $@"
+echo "Value of a is $a"
+echo "scriptName is $0"
+echo "Name of the team is $TEAM"
+echo "Name of the project is $PROJECT"
+echo "Number Of Arguments in script are $#"                             # Prints the no of arguments used in the script 
+echo "Number Of Arguments in script are $@"  
