96632b3f0ece4e39f5f6599471a26a415985e877
dineshcrd <cloudtechnology129@gmail.com>
2025-04-12 08:41:56 +0530

    Bash Scripting

--- 05-SepcialVars.sh
+++ 05-SepcialVars.sh
@@ -5,6 +5,8 @@ TEAM=$1
 PROJECT=$2
 
 echo "The value of a is $a"
-echo "The value of TEAM is $TEAM"
-echo "The value of PROJECT is $PROJECT"
+echo "The name of TEAM is $TEAM"
+echo "The name of PROJECT is $PROJECT"
 echo "script name is $0"
+echo "Number of arguments passed to the script is $#"
+echo "Number of arguments passed to the script is $@"
